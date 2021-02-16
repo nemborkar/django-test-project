3d50bca49599d960e3aeb3380b8104fb0dbdc576
nemborkar <swarnimborkar@gmail.com>
2021-02-16 12:41:13 +0900

    input validations with django forms

--- src/products/views.py
+++ src/products/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
+
 
 def product_create_view(request):
-	form = ProductForm(request.POST or None)
-
-	if form.is_valid():
-		form.save()
-		form = ProductForm()
-
+	my_form = RawProductForm()
+	if request.method == "POST":
+		my_form = RawProductForm(request.POST)
+		if my_form.is_valid():
+			print(my_form.cleaned_data)
+		else:
+			print(my_form.errors)		
 	context = {
-		'form': form
+		"form": my_form
 	}
 	return render(request, "products/product_create.html", context)
 
-# Create your views here.
+
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+
+# 	if form.is_valid():
+# 		form.save()
+# 		form = ProductForm()
+
+# 	context = {
+# 		'form': form
+# 	}
+# 	return render(request, "products/product_create.html", context)
+
 def product_detail_view(request):
 	obj	= Product.objects.get(id=1)
 	context = {
